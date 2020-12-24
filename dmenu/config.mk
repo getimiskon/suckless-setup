be598c2b4c308f65919a1c2157619207031e0700
getimiskon <getimiskon@noreply@snopyta.org>
2020-12-24 14:27:24 +0000

    Update 'dmenu/config.mk'
    Added changes for Void Linux

--- dmenu/config.mk
+++ dmenu/config.mk
@@ -8,6 +8,10 @@ MANPREFIX = $(PREFIX)/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
+# Changes for Void Linux:
+#X11INC = /usr/include/X11
+#X11LIB = /usr/lib/X11
+
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
 XINERAMAFLAGS = -DXINERAMA
