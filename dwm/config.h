f712dcc99c2bb067adfbd72d2c07e34923d206db
getimiskon <getimiskon@noreply@snopyta.org>
2020-12-24 14:21:06 +0000

    Update 'dwm/config.h'

--- dwm/config.h
+++ dwm/config.h
@@ -38,6 +38,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+// In case Firefox launches only in the 9th tag:
 //	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
 };
 
