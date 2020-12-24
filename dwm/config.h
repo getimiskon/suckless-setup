6e9d73ccc6e849f12f020d9fa2af6ab2e93b3560
getimiskon <getimiskon@noreply@snopyta.org>
2020-12-24 14:16:33 +0000

    Update dwm/config.g
    Added a fix for an issue with firefox. Should be changed by user to be enabled.

--- dwm/config.h
+++ dwm/config.h
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Plank",    NULL,	  NULL,	      0,	    0,		 -1 },
+//	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
