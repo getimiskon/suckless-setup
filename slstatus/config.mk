1306dd1ff8bd81676b0c5ae4a92ccfb26e501e80
getimiskon <getimiskon@noreply@snopyta.org>
2020-12-24 14:30:10 +0000

    Update 'slstatus/config.mk'
    Added changes for Void Linux

--- slstatus/config.mk
+++ slstatus/config.mk
@@ -10,6 +10,10 @@ MANPREFIX = $(PREFIX)/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
+# Changes for Void Linux:
+#X11INC = /usr/include/X11
+#X11LIB = /usr/lib/X11
+
 # flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Os
